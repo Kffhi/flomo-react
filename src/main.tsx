8b671fe111a792880a68f708f4ad64363d7c8fb6
kffhi <13979215061@163.com>
2023-02-11 22:59:41 +0800

    build(global): sentry接入

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import App from './views/App/App'
 import store from '@/store'
 
 import 'antd/dist/antd.less'
 import './styles/reset.less'
 import './styles/index.css'
+
+Sentry.init({
+    dsn: 'https://96830828c7c14c03a429f93e3b4d7ab5@o635537.ingest.sentry.io/4504661680193536',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0
+})
 
 ReactDOM.render(
     <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                modifyVars: {
-                    'primary-color': '#30cf79'
-                },
-                additionalData: `@import '@/styles/variables.less';`,
-                javascriptEnabled: true
+export default defineConfig(({ command, mode }) => {
+    return {
+        build: {
+            sourcemap: true
+        },
+        plugins: [
+            react(),
+            sentryVitePlugin({
+                org: 'kffhi-f2e',
+                project: 'flomo-react',
+                ignore: ['node_modules'],
+                urlPrefix: '~/flomo',
+                include: './dist',
+                release: '1.0'
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
             }
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    modifyVars: {
+                        'primary-color': '#30cf79'
+                    },
+                    additionalData: `@import '@/styles/variables.less';`,
+                    javascriptEnabled: true
+                }
+            }
+        },
+        base: './', // 设置打包路径
+        server: {
+            port: 3000, // 设置服务启动端口号
+            open: false, // 设置服务启动时是否自动打开浏览器
+            cors: true // 允许跨域
+            // proxy: {
+            //   '/api': {
+            //     target: 'http://xxx.xxx.xxx.xxx:8000',
+            //     changeOrigin: true,
+            //     secure: false,
+            //     rewrite: (path) => path.replace('/api/', '/')
+            //   }
+            // }
         }
-    },
-    base: './', // 设置打包路径
-    server: {
-        port: 3000, // 设置服务启动端口号
-        open: false, // 设置服务启动时是否自动打开浏览器
-        cors: true // 允许跨域
-        // proxy: {
-        //   '/api': {
-        //     target: 'http://xxx.xxx.xxx.xxx:8000',
-        //     changeOrigin: true,
-        //     secure: false,
-        //     rewrite: (path) => path.replace('/api/', '/')
-        //   }
-        // }
     }
 })
